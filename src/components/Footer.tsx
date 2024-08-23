55d124d74db9890a52f1012e3fc23e8972a9e6bb
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-08-23 17:24:33 +0530

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,79 @@
 export default function Footer(){
-    return <div className="w-full h-[80vh]  flex justify-between items-center bg-zinc-800">
+    return <div className="w-full h-[80vh]  flex flex-col justify-center items-center bg-zinc-900">
+            <div className="w-10/12 h-4/5 text-white  flex">
+                <div className="w-1/3 h-full justify-around py-5 items-start px-5 flex flex-col ">
+                <div className="flex justify-center items-center">
+                        <p className="text-2xl">MyFast</p>
+                        <p className=" text-green-500 text-2xl">X</p>
+                        <img src="https://s3-alpha-sig.figma.com/img/7d8b/18cf/e1c37cf0e56286920a12de54400b8912?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OsolmTvnFe~zMNAVx2GZA~5RxNKArLa9s-bTr9V19~b0gGDTp1QNjwIqRkcfas-~uFZb4C~CTKAV0h~47YlUK~KJ0G1~ChcBaU7Zsj6Ontuj1PI9rRuvareUmmkvRd-30bfa9lEOmCFlUXK6z7zJQyQ4163Jl1iYX7xgn1bBnLNZkXH8a5UgJukgCcMynJjWRN5ljl-HWAYufnG5Ohy4Pug1yyFrZqGo0TSWGZHnQIVcl0gEgegarbcl-QvFr3A8iTgY4-~o5dOBXWoTBca1mSzIf6Crf4~dKrFR5GRcvt~bY--KZ6ZLq3atr~jZrrmXGJGEUbXiqSlsbHIZas6h3Q__"></img>
+        </div>
+                <p className="text-sm text-gray-100">MyFastX is a leading logistics platform that provides hyper-local, on-demand delivery solutions for individuals and businesses. Whether you need a delivery boy for your shop or to ship items items within city, MyFastX is your trusted partner, making delivery affortless and convenient. With a mission to revolutionize logistics through innovation and technology, MyFastX is committed to providing seamless, end-to-end delivery solutions that exceed customer expectations.</p>
+                <div className="text-sm">
+                    <p>GROUND FLOOR, 172/7, PICNIC GARDEN ROAD LP-9/1, Kolkata, West Bengal, 700039</p>
+                </div>
+                <div className="text-sm">
+                    <p>care@myfastx.com</p>
+                    <p>+91 8961310988</p>
+                </div>
+
+                </div>
+                <div className="w-2/3 h-full flex text-sm text-gray-200 justify-between items-center">
+                    <div className="w-1/4 h-full  flex justify-center items-center">
+                        <div className="h-80 w-full  flex flex-col justify-start items-start ">
+                            <p className="text-lg font-medium">Company</p>
+                            <p className="py-2">About Us</p>
+                            <p className="py-2">Partner</p>
+                            <p className="py-2">Investors</p>
+                            <p className="py-2">Press Release</p>
+                            <p className="py-2">Careers</p>
+                            <p className="py-2">Pricing Details</p>
+                        </div>
+
+                    </div>
+                    <div className="w-1/4 h-full  flex justify-center items-center">
+                    <div className="h-80 w-full  flex flex-col justify-start items-start ">
+                            <p className="text-lg font-medium">Policies</p>
+                            <p className="py-2">Terms And Conditions</p>
+                            <p className="py-2">Privacy Policy</p>
+                            <p className="py-2">Cookie Policy</p>
+                            <p className="py-2">Fraud Disclaimer</p>
+                        </div>
+
+                    </div>
+                    <div className="w-1/4 h-full  flex justify-center items-center">
+                    <div className="h-80 w-full  flex flex-col justify-start items-start ">
+                            <p className="text-lg font-medium">Help</p>
+                            <p className="py-2">Restricted Items</p>
+                            <p className="py-2">Parcel size and weight guide</p>
+                            <p className="py-2">Packaging guide</p>
+                        </div>
+
+                    </div>
+                    <div className="w-1/4 h-full  flex justify-center items-center">
+                    <div className="h-80 w-full  flex flex-col justify-start items-start ">
+                            <p className="text-lg font-medium">Support</p>
+                            <p className="py-2">FAQ</p>
+                            <p className="py-2">Customer Care</p>
+                            <p className="py-2">Driver Term and Condition</p>
+                            <p className="py-2">Packaging Instruction</p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <div className="w-10/12 h-1/5 border-y-2 mb-6 border-white flex flex-col text-white justify-center items-start">
+            <p>Our Presence</p>
+            <div className="flex w-1/2 text-sm text-gray-300 justify-between items-center h-1/3">
+                <p>Delhi</p>
+                <p>Noida</p>
+                <p>Greater Noida</p>
+                <p>Ghaziabad</p>
+                <p>Gurugram</p>
+                <p>Faridabad</p>
+                <p>Bahadurgarh</p>
+                <p>Kolkata</p> 
+            </div>
+            </div>
         </div>
 }
--- src/components/stepstostart.tsx
+++ src/components/stepstostart.tsx
@@ -6,29 +6,31 @@ export default function StepstoStart(){
         <p className="bg-gray-100 p-1 border-l-2 border-green-500 text-sm font-medium">How it works</p>
         <p className="text-4xl font-medium border-b-4 border-green-500 pb-2 ">Simple Steps to Get Started</p>
         <div className="h-[80%] flex flex-col justify-around items-center  w-full">
-            <div className="flex justify-around items-center w-full h-1/4 ">
-                <div className="h-14 w-14 rounded-full  border-2 border-dashed border-zinc-400 flex justify-center items-center">
+            <div className="flex justify-center items-center w-full h-1/4 ">
+                <div className="h-14 w-14 rounded-full  border-2 border-dashed border-green-400 flex justify-center items-center">
                     <img src="https://s3-alpha-sig.figma.com/img/a866/a122/ef85a47a90d9f47051092030e0c7d91e?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=j-gJNs-CKJyfVlf~7jcYHMi7HhEJaojCkXAlUZ0F7QKLCPjkMgUIrZquQ4IUSZoiI~8Ngo3NbjfQbHEYsrrDJb3fYEgSvxtMGcqd~rE0DYYvadGqAXBAzpC0roiBI8aXZEIrQvXxkEm1PaVCegcmVbeUkmj-ioJJK0rG0cxmDsCCx7fG7xOuDnHz8okfhy7jcV9BstefMT2iZaQhl9GgitNY4vIrRF6h6s-oO-oatiZ1PAMX67bZ4Ihivjy9tejp9~J~IuqQNlYNCOsEyPXncz1Q0iKVps0BWk8hMFdB4HeieowgSDFuDjzk3v5-hJzv8nLwNzoX3j13zLxIC2z0WQ__" className="h-8 w-8 rounded-full bg-gray-100 object-cover"></img>
-
                 </div>
+                <div className="border-2 border-dashed w-24  "></div>
                 <div className="w-96 h-24 rounded-lg bg-gray-100 px-5 py-3 flex flex-col justify-around items-start">
                     <p className="text-sm text-green-500 font-medium">Register Online</p>
                     <p className="text-sm font-medium">Fill out our easy online application form and pay the registration fee of Rs. 999</p>
                 </div>
             </div>
-            <div className="flex justify-around items-center w-full h-1/4 ">
-                <div className="h-14 w-14 rounded-full  border-2 border-dashed border-zinc-400 flex justify-center items-center">
+            <div className="flex justify-center items-center w-full h-1/4 ">
+                <div className="h-14 w-14 rounded-full  border-2 border-dashed border-green-400 flex justify-center items-center">
                     <img src="https://s3-alpha-sig.figma.com/img/aed3/8100/ec9b542b51128624bcecc8ae6854371f?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Mq6F8f8FCQ51uHcEhB6n3ipyu0jhVSUwRKcVaUnTykQJ28t6r853uigAx0CSExCbXEMtu5myfLd4z5xvIOD5F6DEb~r0JlMXw6~fq7EIRa-8pz12cP1Kv4D0WGfOx8nTX1puEU2dkBDj3mNwzQKktUSHwHngkcD~6luoXtUp7dS~VsOTSbdXWPSr50WNWge5YgArh2pmWR8Mb-b7e-ZSc0o9dbSiKaWdviMnQPVj4Sno9Iwz19vnNjSK-cKXvErAnyOF2FGKjquwmvWS0fJ0FII5Zdo9CoqBao4ilewu~thKx17VyKVrysfIN7ty5mMLBp96FxewcTYrmyzvK5DMtw__" className="h-8 w-8 rounded-full bg-gray-100 object-cover"></img>
                 </div>
+                <div className="border-2 border-dashed w-24  "></div>
                 <div className="w-96 h-24 rounded-lg bg-gray-100 px-5 py-3 flex flex-col justify-around items-start">
                     <p className="text-sm text-green-500 font-medium">Get Verified</p>
                     <p className="text-sm font-medium">Complete the verification process to ensure you meet our standards.</p>    
                 </div>
             </div>
-            <div className="flex justify-around items-center w-full h-1/4 ">
-                <div className="h-14 w-14 rounded-full  border-2 border-dashed border-zinc-400 flex justify-center items-center">
+            <div className="flex justify-center items-center w-full h-1/4 ">
+                <div className="h-14 w-14 rounded-full  border-2 border-dashed border-green-400 flex justify-center items-center">
                     <img src="https://s3-alpha-sig.figma.com/img/e1a5/faca/56682dd6c8b72c126e9397956f9954c4?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iKGez3nfy7V7ZVAAb58uxfa1zDD6q57YL9dxmxg4cwTSGsBtH3AAjMFzqeyMwCndFB8ML1Hkt~QCunjf-MmBLJNG5rMJ5BZTFTtDGAWdC3KJZ3edF3D7nngnu7QgArNo22iO-T-HoJrrCqk5f2b39zvg7lVYxsp3FVtCGHzZFr6QXwiLx3nAN955LYF8fSTjdcTSPaeFwgyHL2ccp8pwtg-pPFSBbz191bEhB~9pg5vXOmMofFN5KV9fRSCWCckuDpA7xwy9ktu81UqCcga~Xr8ME-Bl6QaoSUajJndBpeDGF4d2i2XQDL1zOpkFU6jwYnELyUS82bVWjfCHZiexSQ__" className="h-8 w-8 rounded-full bg-gray-100 object-cover"></img>
                 </div>
+                <div className="border-2 border-dashed w-24  "></div>
                 <div className="w-96 h-24 rounded-lg bg-gray-100 px-5 py-3 flex flex-col justify-around items-start">
                     <p className="text-sm text-green-500 font-medium">Start Driving</p>
                     <p className="text-sm  font-medium">Once approved, start accepting delivery requests and earn money</p>
